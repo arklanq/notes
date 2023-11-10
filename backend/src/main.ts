be18fbfed1212badf57a7985a4c609fda11e5175
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-10 16:29:29 +0100

    feat(backend): apply cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,18 @@ import {NotesModule} from './notes.module';
 import {errorMap} from 'zod-validation-error';
 import {z} from 'zod';
 
+// Before you start reviewing, please see
+// https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
 async function bootstrap(): Promise<void> {
   // Provide custom message mapping for Zod issues
   z.setErrorMap(errorMap);
 
   const app = await NestFactory.create(NotesModule);
+  app.enableCors({
+    // Only for development
+    origin: '*'
+  });
   await app.listen(8000);
 }
 
