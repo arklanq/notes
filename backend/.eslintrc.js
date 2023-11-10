57e2bfc4528ebabc4fb7f403c648677f4ddd19d7
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-10 14:12:58 +0100

    feat: scaffold nest.js backend and next.js frontend app

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    warnOnUnsupportedTypeScriptVersion: true
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended'
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {},
+};
