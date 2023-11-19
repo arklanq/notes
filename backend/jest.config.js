074ba34b199b63501a4ae27ce33eda0dda007e88
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-19 11:36:57 +0100

    fix(backend): intellisense based IDE's complaining about unknown jest matchers

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest'
   },
+  // setupFiles
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node'
