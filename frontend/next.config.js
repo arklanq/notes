8641e3ed9fc260f5cf74463fe8396d9a06f268c0
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-19 15:10:08 +0100

    feat(frontend): implement `next-recompose-plugins` package for next.js's plugins management

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,8 @@ const {Config} = require('next-recompose-plugins');
 const {PHASE_PRODUCTION_BUILD} = require('next/constants');
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 
-/** @type {import("next").NextConfig} */
-const config = new Config(async () => {
+const config = new Config(async (_phase, _args) => {
+  /** @type {import("next").NextConfig} */
   return {
     reactStrictMode: true,
     sassOptions: {
