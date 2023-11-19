91392d2886126d0443ae2c5b60dbed528812f0bd
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-19 11:38:14 +0100

    feat(frontend): implement `@next/bundle-analyzer` plugin via `next-recompose-plugins` package

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const {Config} = require('next-recompose-plugins');
+const {PHASE_PRODUCTION_BUILD} = require('next/constants');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')]
-  },
-};
+const config = new Config(async () => {
+  return {
+    reactStrictMode: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'src/styles')]
+    },
+  };
+})
+  .applyPlugin((phase, args, config) => {
+    return withBundleAnalyzer({enabled: phase === PHASE_PRODUCTION_BUILD})(config);
+  }, 'bundle-analyzer')
+  .build();
 
-module.exports = nextConfig;
+module.exports = config;
