4903126fdb984478f349dad47493de95706027d9
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-11-22 23:11:37 +0100

    feat(backend): complete tests configuration overhaul

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,33 @@
-/** @type {import('jest').Config} */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+const path = require('node:path');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest'
-  },
-  // setupFiles
+  /*
+   * General configuration
+   */
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'], // order matters
+  testRegex: '.*\\.test\\.ts$',
+  testMatch: null, // Must be set to null, see https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
+  roots: ['<rootDir>/src', '<rootDir>/test'], // can be explicitly overriden by CLI option
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node'
+  cacheDirectory: '<rootDir>/node_modules/.cache/jest',
+
+  /*
+   * TypeScript configuration
+   */
+  preset: 'ts-jest',
+  modulePaths: [path.join('<rootDir>', compilerOptions.baseUrl)], // path aliases configuration
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },  // path aliases configuration
+
+  /*
+   * Coverage configuration
+   */
+  collectCoverage: false, // can be explicitly overriden by CLI option
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage'
 };
 
 module.exports = config;
--- backend/test/e2e/app.test.ts
+++ backend/test/e2e/app.test.ts
@@ -1,7 +1,7 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {INestApplication} from '@nestjs/common';
 import * as request from 'supertest';
-import {NotesModule} from '../src/notes.module';
+import {NotesModule} from '../../src/notes.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,7 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  test('/notes (GET)', () => {
+    return request(app.getHttpServer()).get('/notes').expect(200).expect([]);
   });
 });
