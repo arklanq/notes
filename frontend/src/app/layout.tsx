9001472936269f2f854ba3dc4bf26120a282f689
Arkadiusz Nurzyński <nowyarek@gmail.com>
2024-01-13 22:12:48 +0100

    feat(frontend): replace default font with Raleway

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,29 +1,37 @@
 import './global.css';
 import type {Metadata} from 'next';
 import {PropsWithChildren} from 'react';
-import {NextFont} from 'next/dist/compiled/@next/font';
-import {Roboto} from 'next/font/google';
+import {Raleway} from 'next/font/google';
 import PageShell from '@/components/organisms/PageShell/PageShell';
+import { NextFontWithVariable } from 'next/font';
 
 export const metadata: Metadata = {
   title: 'Notes',
   description: 'Yet another note taking app'
 };
 
-const robotoFont: NextFont = Roboto({
+const RalewayFont: NextFontWithVariable = Raleway({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '700']
+  weight: [
+    '300', // 'light'
+    '400', // 'regular'
+    '500', // 'medium'
+    '600', // 'semibold'
+    '700' // 'bold'
+  ],
+  style: ['normal'],
+  variable: '--body-font'
 });
 
 export default function RootLayout(props: PropsWithChildren) {
   const {children} = props;
 
   return (
-    <html lang='en'>
+    <html lang='en' className={[RalewayFont.variable, RalewayFont.className].join(' ')}>
       <head>
         <link key={'favicon'} rel='icon' type='image/png' sizes='48x48' href='favicon-48x48.png' />
       </head>
-      <body className={robotoFont.className}>
+      <body>
         <PageShell>{children}</PageShell>
       </body>
     </html>
