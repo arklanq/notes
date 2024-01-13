22536477cb2c9dd5746f47d0e9fc81f59d20ea60
Arkadiusz Nurzyński <nowyarek@gmail.com>
2024-01-13 22:08:06 +0100

    feat(frontend): tweak tailwind, postcss and typescript configuration

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  plugins: {
+const plugins = {
+  base: {
     'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss': {}
   },
-}
+  productionOnly: {
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false }
+    },
+    cssnano: {}
+  }
+};
+
+module.exports = {
+  plugins: Object.assign({}, plugins.base, process.env.NODE_ENV === 'production' && plugins.productionOnly)
+};
